ae57273cee99e8123fef18df5a0e5e032faaae50
Pedro Sánchez Castro <pedro@sanchez.com>
2018-02-01 19:36:43 +0100

    First FlatList, mock heroes

--- src/sections/heroes/HeroesList.js
+++ src/sections/heroes/HeroesList.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 
 export default class HeroesList extends Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            list: ['Adam destiny','3D-Man','Abbobination','Abbys']
+        }
+    }
+
+    renderItem(item) {
+        return(
+        <View style = {{ height: 200, backgroundColor: 'grey', marginVertical: 10 }}>
+            <Text>{ item }</Text>
+        </View>
+        )
+    }
+
     render() {
         return(
-        <View>
-            <Text> Heroes List Component </Text>
+            <View>
+            <FlatList
+                data={this.state.list}
+                renderItem={ ({item}) => this.renderItem(item) }
+            />
         </View>
         )
     }
